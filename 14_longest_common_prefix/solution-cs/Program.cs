a75a43a70a9e8aa5581a46458a9b65b6c0ac3d08
Austen Harrell <harrell1st@gmail.com>
2025-08-15 12:37:43 -0600

    Complete 14: Longest Common Prefix with csharp implementation

---
+++ 14_longest_common_prefix/solution-cs/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string prefix = "";
+        if (strs.Length == 0) {
+            return prefix;
+        }
+
+        for (int i = 0; i < strs[0].Length; i++) {
+            char currentLetter = strs[0][i];
+            for (int j = 0; j < strs.Length; j++) {
+                if ((strs[j].Length <= i) || (strs[j][i] != currentLetter)) {
+                    return prefix;
+                }
+            }
+            prefix += currentLetter;
+        }
+        return prefix.ToString();
+    }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        Solution prefix = new Solution();
+        string[] words1 = {"flower", "flow", "flight"};
+        string result1 = prefix.LongestCommonPrefix(words1);
+        Console.WriteLine(result1);
+
+        string[] words2 = {"dog", "racecar", "car"};
+        string result2 = prefix.LongestCommonPrefix(words2);
+        Console.WriteLine(result2);
+    }
+}
