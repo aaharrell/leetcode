2df0fec909b7dfea69ca90c430a9cdd84cf94270
Austen Harrell <harrell1st@gmail.com>
2025-08-25 14:26:49 +0200

    cpp implementation complete

---
+++ 0001_two-sum/solution-cpp/solution.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::unordered_map;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> solutionMap;
+        for (int i =0; i < nums.size(); i++) {
+            int needed = target - nums[i];
+            if (solutionMap.count(needed)) {
+                return {solutionMap[needed], i};
+            } else {
+                solutionMap[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    return 0;
+}
